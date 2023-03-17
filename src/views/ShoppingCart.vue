52ac602b26ec16b67ce334d7d63ddb3a402b3894
stklue <st.klue09uct@gmail.com>
2023-03-17 09:33:39 +0200

    fixed: cart component

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -3,9 +3,9 @@ import "/src/index.css";
 import CartOrderSummary from "../components/CartOrderSummary.vue";
 import Product from '../utilities/products';
 
-const item1 = {id: 1, name: "Furniture 1", image: "/src/assets/images/fur-3.jpg", price: 300, quantity: 2, category: "Furniture"} as Product;
-const item2 = {id: 2, name: "Decor Item 4", image: "/src/assets/images/decor-2.jpg", price: 1300, quantity: 2, category: "Decor"} as Product;
-const item3 = {id: 3, name: "Kitchenware 2", image: "/src/assets/images/kit-3.jpg", price: 200, quantity: 2, category: "Kitchenware"} as Product;
+const item1 = { id: 1, name: "Furniture 1", image: "/src/assets/images/fur-3.jpg", price: 300, quantity: 2, category: "Furniture" } as Product;
+const item2 = { id: 2, name: "Decor Item 4", image: "/src/assets/images/decor-2.jpg", price: 1300, quantity: 2, category: "Decor" } as Product;
+const item3 = { id: 3, name: "Kitchenware 2", image: "/src/assets/images/kit-3.jpg", price: 200, quantity: 2, category: "Kitchenware" } as Product;
 
 const cartItems = [item1, item2, item3]
 </script>
@@ -14,59 +14,96 @@ const cartItems = [item1, item2, item3]
 <template>
   <div class="cart-wrapper">
     <div class="cart-content-wrapper">
-      <div class="cart-left-items">
+      <div class="cart-left-items space-y-2">
         <div class="cart-left-items-text">
           <h1>Shopping Cart</h1>
           <h2>
-            3 Item(s)
+            2 Item(s)
           </h2>
         </div>
 
 
-        <div v-for="item in cartItems" :key="item.id">
-          <div class="flex items-center hover:bg-gray-100 mx-1 my-2 h-28 space-x-5 ">
-            <!-- Image -->
-            <div class="flex h-full w-1/3 lg:w-1/6 p-2">
-              <img :src="item.image" />
-            </div>
-        
-            <!-- Product details -->
-            <div class="flex h-full w-2/3 lg:w-full flex-col justify-between">
-              <div class="flex flex-col justify-between">
-                <!-- <router-link :to="`/brand/${item.cat}/${item.id}`"> -->
-                <span class="font-bold text-sm">{{ item.name }}</span>
-                <!-- </router-link> -->
-                <div class="flex space-x-5 lg:justify-start">
-                  <span class="text-center font-semibold text-xs text-gray-400">R{{ item.price }}</span>
-                  <!-- <span class="text-red-500 text-xs">{{ item.data.size }}</span> -->
-                </div>
+
+        <div class="flex pb-9 items-center hover:bg-gray-100 mx-1 my-2 h-24 space-x-5">
+          <!-- Image -->
+          <div class="flex h-full w-1/3 lg:w-1/6 p-2">
+            <img class="product-img" src="../assets/images/fur-2.jpg" alt="" />
+          </div>
+
+          <!-- Product details -->
+          <div class="flex h-full w-2/3 lg:w-full flex-col justify-between">
+            <div class="flex flex-col justify-between">
+              <!-- <router-link :to="`/brand/${item.cat}/${item.id}`"> -->
+              <span class="font-bold text-sm">Furniture 2</span>
+              <!-- </router-link> -->
+              <div class="flex space-x-5 lg:justify-start">
+                <span class="text-center font-semibold text-xs text-gray-400">R1200.00</span>
+                <!-- <span class="text-red-500 text-xs">{{ item.data.size }}</span> -->
               </div>
-        
-              <!-- Incr and Decr and Trash/Remove -->
-              <div class="flex justify-between pr-5">
-                <div class="flex">
-                  <svg class="fill-current text-gray-600 w-5 h-5 rounded-lg border-2 p-1" viewBox="0 0 448 512">
-                    <path
-                      d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                  </svg>
-        
-                  <span class="text-center w-8">{{ item.quantity }}</span>
-        
-                  <svg class="fill-current text-gray-600 w-5 h-5 rounded-lg border-2 p-1" viewBox="0 0 448 512">
-                    <path
-                      d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                  </svg>
-                </div>
-                <div>
-                  <a class="delete font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
-                </div>
+            </div>
+
+            <!-- Incr and Decr and Trash/Remove -->
+            <div class="flex justify-between pr-5">
+              <div class="flex">
+                <svg class="fill-current text-gray-600 w-5 h-5 rounded-lg border-2 p-1" viewBox="0 0 448 512">
+                  <path
+                    d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                </svg>
+
+                <span class="text-center w-8">2</span>
+
+                <svg class="fill-current text-gray-600 w-5 h-5 rounded-lg border-2 p-1" viewBox="0 0 448 512">
+                  <path
+                    d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                </svg>
+              </div>
+              <div>
+                <a class="delete font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="flex items-center hover:bg-gray-100 mx-1 my-2 h-24 space-x-5 pt-20">
+          <!-- Image -->
+          <div class="flex h-full w-1/3 lg:w-1/6 p-2">
+            <img class="product-img" src="../assets/images/decor-2.jpg" alt="" />
+          </div>
+          <div class="flex h-full w-2/3 lg:w-full flex-col justify-between">
+            <div class="flex flex-col justify-between">
+              <!-- <router-link :to="`/brand/${item.cat}/${item.id}`"> -->
+              <span class="font-bold text-sm">Decor Item 2</span>
+              <!-- </router-link> -->
+              <div class="flex space-x-5 lg:justify-start">
+                <span class="text-center font-semibold text-xs text-gray-400">R400.00</span>
+                <!-- <span class="text-red-500 text-xs">{{ item.data.size }}</span> -->
+              </div>
+            </div>
+
+            <!-- Incr and Decr and Trash/Remove -->
+            <div class="flex justify-between pr-5">
+              <div class="flex">
+                <svg class="fill-current text-gray-600 w-5 h-5 rounded-lg border-2 p-1" viewBox="0 0 448 512">
+                  <path
+                    d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                </svg>
+
+                <span class="text-center w-8">3</span>
+
+                <svg class="fill-current text-gray-600 w-5 h-5 rounded-lg border-2 p-1" viewBox="0 0 448 512">
+                  <path
+                    d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+                </svg>
+              </div>
+              <div>
+                <a class="delete font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</a>
               </div>
             </div>
           </div>
         </div>
 
         <!-- Continue shopping routing -->
-        <router-link :to="{ name: 'Home' }" class="flex font-semibold text-[#382a1d] text-sm mt-10">
+        <router-link :to="{ name: 'Home' }" class="flex font-semibold text-[#382a1d] text-sm mt-10 pt-44">
           <svg class="fill-current mr-2 text-[#382a1d] w-4" viewBox="0 0 448 512">
             <path
               d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
